c73fa22a84c69a4d105f37fc3189b090f08f2635
Michael <mdaniels6532@gmail.com>
2019-05-20 21:35:17 -0400

    first commit to node-lab

---
+++ quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+    'Before you judge a man, walk a mile in his shoes. After that who cares?... He’s a mile away and you’ve got his shoes! --Billy Connolly',
+    'People say nothing is impossible, but I do nothing every day. – A. A. Milne',
+    'The only mystery in life is why the kamikaze pilots wore helmets. – Al McGuire',
+    'Light travels faster than sound. This is why some people appear bright until you hear them speak. – Alan Dundes',
+    'Nobody realizes that some people expend tremendous energy merely to be normal.',
+    'If you want your children to listen, try talking softly to someone else. – Ann Landers',
+    'My opinions may have changed, but not the fact that I’m right. – Ashleigh Brilliant',
+    'Money won’t buy happiness, but it will pay the salaries of a large research staff to study the problem. – Bill Vaughan',
+    'The surest sign that intelligent life exists elsewhere in the universe is that it has never tried to contact us. – Bill Watterson',
+    'A bank is a place that will lend you money if you can prove that you don’t need it. – Bob Hope',
+    'Inside me there’s a thin person struggling to get out, but I can usually sedate him with four or five cupcakes. – Bob Thaves',
+    'We never really grow up, we only learn how to act in public. – Bryan White',
+    'As a child my family’s menu consisted of two choices: take it or leave it. – Buddy Hackett'
+];
+
+module.exports= {
+    quotes
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+// "use strict";
+
+// const http = require("http");
+
+// const express = require('express')
+// const app= express();
+// // const quotes = [
+// //     'age= ?',
+// //     'eyes= brown',
+// //     'weight= 200+',
+// //     'shoe size= 11'
+// // ];
+ 
+// http
+//   .createServer((req,res) => {
+//       res.writeHead(300, {"Content-type": "test/plain"});
+//       res.write(quotes[Math.floor(Math.random() * quotes.length)]);
+//       res.end();
+//   })
+//   .listen(3000);
